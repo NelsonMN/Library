dd0830ea6025a6367111a55b7b63682a65085971
Nelson <nelson.mieszkalski@gmail.com>
2022-01-20 17:03:58 -0500

    Added a few JS functions and some stylings

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let myLibrary = [];
+let myLibrary = [
+    {title: "The Great Gatsby", author: "F. Scott Fitzgerald", pageCount: "208", read: "no"},
+    {title: "Nineteen Eighty-Four", author: "George Orwell", pageCount: "328", read: "no"},
+    {title: "To Kill A Mockingbird", author: "Harper Lee", pageCount: "281", read: "yes"},
+    {title: "The 5 AM Club", author: "Robin Sharma", pageCount: "336", read: "yes"},
+    {title: "Atomic Habits", author: "James Clear", pageCount: "320", read: "yes"}
+];
 
 function Book(title, author, pageCount, read) {
     this.title = title
@@ -7,7 +13,12 @@ function Book(title, author, pageCount, read) {
     this.read = read
 }
 
-function addBookToLibrary(book) {
+const addBook = function(book) {
     myLibrary.push(book)
 }
 
+const displayBooks = function() {
+    for (i = 0; i < myLibrary.length; i++) {
+        return myLibrary[i]
+    }
+}
